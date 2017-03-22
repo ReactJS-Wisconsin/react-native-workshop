45172be8524ca180065a1cbab66c85085be07a4c
ezekielchentnik <zeek@ezekielchentnik.com>
2017-03-22 14:47:59 -0500

    update

--- src/step-8/Finish.js
+++ src/step-8/Finish.js
@@ -7,8 +7,13 @@ class Finish extends Component {
     return(
       <div>
         <h1>8. Finish</h1>
-        [congrats]
-        [homeowrk]
+        <p>Congrats you are done.  If you have gotten this far because you are super fast, try implementing a <a href="https://facebook.github.io/react-native/docs/mapview.html">Map component!</a>   </p>
+        <h2>Home Work</h2>
+        <ul>
+          <li>Follow the complete get starting guide by Facebook - <a href="https://facebook.github.io/react-native/docs/getting-started.html">Guide</a></li>
+          <li>Bring questions for the next session.</li>
+          <li>Be sure to vote on the App to build next time.</li>
+        </ul>
       </div>
     );
   }
