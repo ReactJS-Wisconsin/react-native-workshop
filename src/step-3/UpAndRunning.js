8b41e0390dc6de811491020b151e9ec776436b73
ezekielchentnik <zeek@ezekielchentnik.com>
2017-03-22 09:35:01 -0500

    update

--- src/step-3/UpAndRunning.js
+++ src/step-3/UpAndRunning.js
@@ -4,86 +4,12 @@ import Highlight from 'react-syntax-highlight';
 
 class UpAndRunning extends Component {
   render() {
-    const appShellCode =
-    `<!-- Header  -->
-<header class="header">
-  <div class="header__container">
-    <div class="header__icon">
-      <!-- Header Icon  -->
-    </div>
-    <h1>Title</h1>
-  </div>
-</header>
-
-<!-- Menu  -->
-<div class="menu">
-  <div class="menu__header"></div>
-  <ul class="menu__list">
-    <li><a href="/">Home</a></li>
-  <ul>
-</div>
-
-<!-- Menu Overlay -->
-<div class="menu__overlay"></div>
-
-<!-- Main Section -->
-<div class="main"></div>`;
-
+  
     return(
       <div className="general">
         <h1>3. Up & Running</h1>
-        <p>An app shell architecture is minimal level <span className="highlight bold no--bg">HTML, CSS, Javascript</span> required to power the application user interface.</p>
-
-
-        <p>Calls to console.log are forwarded to your terminal. You can make use of any standard React Native APIs as well as the Expo SDK.</p>
-
-
-        <h2>Components for App Shell</h2>
-        <ul className="">
-          <li>Header with icon and title</li>
-          <li>Menu with overlay</li>
-          <li>Main section</li>
-        </ul>
-
-        <h2>HTML for App Shell</h2>
-
-        <Highlight lang='html' value={appShellCode} />
-
-        <i><b>Screenshot:</b></i>
-
-        <div className="offline__container">
-          <img alt="app shell" />
-        </div>
-
-        <h1>Performance</h1>
-        <p>By caching the app shell, repeated visits on the application loads faster. To measure the performance of the app shell, we did some series of tests.</p>
-
-        <b>Test 1: DevTools</b>
-        <p>Lets emulate 3G connection in DevTools. On the repeated visits, app shell was loaded within a few <span className="highlight bold no--bg">milli seconds</span>.</p>
-
-        <div className="offline__container">
-          <img  alt="devTools network panel" />
-        </div>
-
-        <b>Test 2: Webpagetest</b>
-        <p>In webpagetest, we measured the same site in <span className="highlight bold no--bg">Chrome - 3G</span>. Load time for repeat visits was <span className="highlight bold no--bg">3.015s</span> Results are below.</p>
-
-        <div className="offline__container">
-          <img alt="web page test" />
-        </div>
-
-        <p>See the full result <a href="http://www.webpagetest.org/result/161021_1H_G6C/">here</a>.</p>
-
-        <h2>Applications using app shell in production</h2>
-        <ul>
-          <li><a href="https://www.google.com/inbox/" target="_blank">Inbox</a> by Gmail</li>
-          <li><a href="flipkart.com" target="_blank">Flipkar Lite</a></li>
-          <li><a href="housing.com" target="_blank">Housing.com</a> mobile site</li>
-        </ul>
-
-        <Note type="facts">
-          <p><span>Facts: </span> Using app shell model, <a href="housing.com" target="_blank">Housing.com</a> were able to load the page 30% faster than before. Read thier full case study <a href="https://developers.google.com/web/showcase/2016/pdfs/housing.pdf" target="_blank">here</a>.</p>
-        </Note>
+[tips during dev]
+[change text]
       </div>
     );
   }
--- src/step-8/Finish.js
+++ src/step-8/Finish.js
@@ -4,36 +4,11 @@ import Note from '../Note';
 
 class Finish extends Component {
   render() {
-    const surgeInstall = `$ npm install --global surge`;
-    const finalDir = `$ cd final`;
-    const surgeDeploy = `$ surge`;
-    const surgeDeployHTTPS = `$ surge --domain https://my-project.surge.sh`;
     return(
       <div>
         <h1>8. Finish</h1>
-        <p>The final step of this workshop is deploying our github cards with HTTPS server.</p>
-        <h2>Follow the below steps.</h2>
-        <p><span className="highlight bold no--bg">Step 1</span> - Install surge via npm.</p>
-        <Highlight lang="bash" value={surgeInstall} />
-
-        <p><span className="highlight bold no--bg">Step 2</span> - Go to <span className="highlight bold no--bg">final directory</span> in sample repository.</p>
-        <Highlight lang="bash" value={finalDir} />
-
-        <p><span className="highlight bold no--bg">Step 3</span> - Type the below command to deploy.</p>
-        <Highlight lang="bash" value={surgeDeploy} />
-
-        <p>After successful deployment, you will get an url in your terminal. Copy it and open in your desktop and mobile browsers :D</p>
-
-        <h2>What next?</h2>
-        <p>Everything is perfect, except the deployed site is loading in HTTP unless we change the url to HTTPS. So lets fix it by forcing HTTP to redirect to HTTPS.</p>
-
-        <Highlight lang="bash" value={surgeDeployHTTPS} />
-
-        <Note type="tips">
-          <p><span>Tips: </span> <a href="https://chrome.google.com/webstore/detail/blipmdconlkpinefehnmjammfjpmpbjk" target="_blank">Lighthouse</a> analyzes web apps and web pages, collecting modern performance metrics and insights on developer best practices. Our application score was <span className="highlight bold no--bg">91/100</span>.</p>
-        </Note>
-
-        <p>Thats it all done!!</p>
+        [congrats]
+        [homeowrk]
       </div>
     );
   }
