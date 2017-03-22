3f7a84f9fc91df37462494ca76065f043d2b82f6
ezekielchentnik <zeek@ezekielchentnik.com>
2017-03-22 14:11:14 -0500

    update

--- src/step-1/Introduction.js
+++ src/step-1/Introduction.js
@@ -8,6 +8,10 @@ class Introduction extends Component {
         <h1>1. Introduction</h1>
         <p>In this workshop, you will be learning about React Native, how it works, and how to build your first React Native App.  We will be using the newly released Create React Native App.</p>
 
+        <h2>Who is this workshop for?</h2>
+<p>In short, Everyone!  Beginner or Guru.  In workshop 1 we keep things basic to get everyone up to speed.  If you are an advanced React Native or React developer, this is your time to help out your fellow workshop buddies!</p>
+
+<p>At the end of this workshop, we will discuss where we want to take workshop 2 and vote on an App to do deeper dive on and build (Realtime Chat, Twitter Clone, etc.)</p>
         <h2>Why Create React Native App?</h2>
 
           <p>Create React Native App should reduce the setup time it takes to try building a mobile app to the absolute minimum, ideally on par with React web development (especially as seen with Create React App).
@@ -16,8 +20,8 @@ Creact React Native App runs Expo under the hood.
 
           <p><a href="https://github.com/react-community/create-react-native-app#faqs">See Create React Native App FAQS</a></p>
 
-          <p>Let{`'`}s get started!</p>
-
+          <h2>Let{`'`}s get started!</h2>
+  <Note type="tips"><p><span>Tip: </span> .</p></Note>
           <h2>Helpful Links</h2>
           <ul>
             <li><a href="https://facebook.github.io/react-native/releases/next/docs/getting-started.html">React Native Component Docs</a></li>
--- src/step-3/UpAndRunning.js
+++ src/step-3/UpAndRunning.js
@@ -2,14 +2,49 @@ import React, {Component} from 'react';
 import Note from '../Note';
 import Highlight from 'react-syntax-highlight';
 
+const editText =`// App.js
+// ..
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Edit some text to see your app work!</Text> // heyo!
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+// ...
+
+`;
+
 class UpAndRunning extends Component {
   render() {
-  
+
     return(
       <div className="general">
         <h1>3. Up & Running</h1>
-[tips during dev]
-[change text]
+
+<p>Time to start editing code and get familiar with your dev environment.</p>
+        <ul className="setup__steps">
+<li>Open your newly created app inside your favorite text editor</li>
+<li>Take a peek inside package.json to see your apps dependancies and scripts</li>
+<li>Open up App.js and edit the text, save, and see your changes hot reload on your phone
+          <Highlight lang='javascript' value={editText} />
+</li>
+</ul>
+<h2>Common commands</h2>
+<ul>
+<li>`Ctrl + c` to stop the app </li>
+<li>`npm start` to fire the app up again</li>
+</ul>
+
+
+<Note type="facts"><p><span>Note: </span> If you are having trouble, ask the guy next to you for help!  Do not sit stuck :)</p></Note>
+
+
       </div>
     );
   }
